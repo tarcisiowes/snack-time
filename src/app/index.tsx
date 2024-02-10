43ffd57dbbc3036be564523cab9139756719149a
tarushii <tarcisiowesley@gmail.com>
2024-02-10 14:39:25 -0300

    feat: added CategoryButton to home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import{View, Text} from 'react-native';
 import {Header} from "@/components/header";
+import {CategoryButton} from "@/components/category-button";
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" />
+        <Header title="Faça seu pedido" />
+        <CategoryButton title={'Lanche do dia'} />
     </View>
   );
 }
