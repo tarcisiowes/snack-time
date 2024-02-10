8219917b9a887cf908ba81e6e08b7c1ca42552d2
tarushii <tarcisiowesley@gmail.com>
2024-02-10 14:37:31 -0300

    style: changed header image style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header({title}: HeaderProps) {
     return (
         <View className="flex-row items-center border-b border-amber-300 pb-5 mx-5">
             <View className="flex-1">
-                <Image source={require('@/assets/logo/logo-medium-black-transparent.png')} className="w-32 h-6" />
+                <Image source={require('@/assets/logo/logo-small-black-transparent.png')} className="w-32 h-11 absolute bottom-8 left-1" />
                 <Text className="text-white text-xl font-heading mt-2">{title}</Text>
             </View>
 
