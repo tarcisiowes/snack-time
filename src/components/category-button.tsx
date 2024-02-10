a31fb96d20883b6f8f6b162c21ef1a595d0942a4
tarushii <tarcisiowesley@gmail.com>
2024-02-10 14:29:21 -0300

    feat: added CategoryButton type

--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -1,6 +1,11 @@
 import {Text, Pressable} from "react-native";
 
-export function CategoryButton({title}) {
+type CategoryButtonProps = {
+    title: string;
+    isSelect?: boolean;
+}
+
+export function CategoryButton({title} : CategoryButtonProps) {
     return (
         <Pressable className="flex-row items-center bg-white rounded-lg p-2 mx-2">
             <Text >{title}</Text>
