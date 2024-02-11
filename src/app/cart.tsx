8cb1745999a7bdac746bc2070fc7dad521a4c3fa
tarushii <tarcisiowesley@gmail.com>
2024-02-11 19:18:31 -0300

    feat: added scrollview with map to display products

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -10,6 +10,13 @@ export default function CartScreen() {
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho" />
+            <ScrollView className="flex-1">
+                <View className="flex-1 p-5">
+                    {cartStore.products.map((product) => (
+                        <Product data={product} key={product.id} />
+                    ))}
+                </View>
+            </ScrollView>
 
         </View>
     );
