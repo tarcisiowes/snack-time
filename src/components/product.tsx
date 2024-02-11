de2f8f274c9708dce18cda740ac31fab40159be6
tarushii <tarcisiowesley@gmail.com>
2024-02-11 11:15:34 -0300

    style: added style and type to product component

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,10 +1,10 @@
-import {TouchableOpacity, TouchableOpacityProps} from "react-native";
+import {Image, ImageProps, Text, TouchableOpacity, TouchableOpacityProps, View} from "react-native";
 
 type ProductDataProps =  {
     title: string;
     description: string;
     price: number;
-    thumbnail: string;
+    thumbnail: ImageProps;
 }
 
 type ProductProps = TouchableOpacityProps & {
@@ -13,8 +13,12 @@ type ProductProps = TouchableOpacityProps & {
 
 export function Product({data, ...rest}: ProductProps) {
   return (
-      <TouchableOpacity>
-
+      <TouchableOpacity className="w-full flex-row items-center pb-4" {...rest}>
+        <Image source={data.thumbnail} className="w-35 h-35 rounded-md" />
+            <View className="flex-1 ml-3" >
+            <Text className="text-slate-900 font-subTitle text-base flex-1">{data.title}</Text>
+            <Text className="text-slate-800 text-sm leading-5 mt-0.5">{data.description}</Text>
+        </View>
       </TouchableOpacity>
   );
 }
