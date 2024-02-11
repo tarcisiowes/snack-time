c5f276f5c22b0c5e22461eea23889536696494fb
tarushii <tarcisiowesley@gmail.com>
2024-02-11 12:49:26 -0300

    feat: created string helper and function formatCurrency

---
+++ src/utils/string-helper/format-currency.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: number, currency = "BRL") {
+    const formatter = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency,
+        minimumFractionDigits : 2,
+    });
+    return formatter.format(value);
+}
