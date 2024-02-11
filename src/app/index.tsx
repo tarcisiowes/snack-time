791b8dc112a829c7f93c26a9257605bebf69b07f
tarushii <tarcisiowesley@gmail.com>
2024-02-11 12:51:37 -0300

    feat: added link from expo route to navigate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ import {CategoryButton} from "@/components/category-button";
 import {CATEGORIES, MENU} from "@/utils/data/products";
 import {useRef, useState} from "react";
 import {Product} from "@/components/product";
+import {Link} from "expo-router";
 
 export default function Home() {
     const [selectedCategory, setSelectedCategory] = useState(CATEGORIES[0])
@@ -50,8 +51,10 @@ export default function Home() {
             keyExtractor={(item ) => item.id}
             stickySectionHeadersEnabled={false}
             renderItem={({item}) => (
-                <Product data={item} />)
-            }
+                <Link href={`/product/${item.id}`} asChild>
+                    <Product data={item} />
+                </Link>
+            )}
             renderSectionHeader={({section: {title}}) => (
                 <Text className="text-xl text-slate-900 font-heading mt-8 mb-3">{title}</Text>
             )}
