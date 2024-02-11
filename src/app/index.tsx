23149b7db4128fea9e1c5df65c157487b7642da6
tarushii <tarcisiowesley@gmail.com>
2024-02-11 10:25:42 -0300

    feat: created selected category state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,11 @@ import {View, Text, FlatList} from 'react-native';
 import {Header} from "@/components/header";
 import {CategoryButton} from "@/components/category-button";
 import {CATEGORIES} from "@/utils/data/products";
-
+import {useState} from "react";
 
 export default function Home() {
+    const [selectedCategory, setSelectedCategory] = useState(CATEGORIES[0])
+
   return (
     <View className="flex-1 pt-8">
         <Header title="Faça seu pedido" />
