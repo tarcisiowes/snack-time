5a3cf64de6f79415eb17bf421a8076b520ef8e5b
tarushii <tarcisiowesley@gmail.com>
2024-02-11 19:36:27 -0300

    feat: created input text to get address

---
+++ src/components/input.tsx
@@ -0,0 +1,15 @@
+import {TextInput, TextInputProps} from "react-native";
+import colors from "tailwindcss/colors";
+
+export function Input({ ...rest}: TextInputProps) {
+    return (
+        <TextInput
+            multiline
+            textAlignVertical="top"
+            placeholder="Bairro, rua, número e complemento"
+            placeholderTextColor={colors.slate[500]}
+            className="h-32 px-4 py-3 bg-white border-2 border-amber-600 rounded-md text-amber-950 text-sm font-body"
+            {...rest}
+        />
+    );
+}
