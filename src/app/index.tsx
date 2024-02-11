417c877dd8ca837db9aea2e519434a22350b0c61
tarushii <tarcisiowesley@gmail.com>
2024-02-11 18:38:13 -0300

    feat: added type

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,12 +6,12 @@ import {Header} from "@/components/header";
 import {CategoryButton} from "@/components/category-button";
 import {Product} from "@/components/product";
 import {useCartStore} from "@/data-store/cart-store";
-import {CATEGORIES, MENU} from "@/utils/data/products";
+import {CATEGORIES, MENU, ProductProps} from "@/utils/data/products";
 
 export default function Home() {
     const cartStore = useCartStore()
     const [selectedCategory, setSelectedCategory] = useState(CATEGORIES[0])
-    const sectionListRef = useRef<SectionList>(null)
+    const sectionListRef = useRef<SectionList<ProductProps>>(null)
     const cartQuantityItems = cartStore.products.reduce(
         (total, product) =>
             total + product.quantity, 0)
