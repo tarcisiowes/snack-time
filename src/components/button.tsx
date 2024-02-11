043a6778c509a8d36872554ce9d91077334432e7
tarushii <tarcisiowesley@gmail.com>
2024-02-11 14:38:14 -0300

    style: changed to green color

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -16,7 +16,7 @@ type ButtonIconProps = {
 function Button({ children, ...rest }: ButtonProps) {
   return (
       <TouchableOpacity
-          className="h-12 bg-lime-400 rounded-md items-center justify-center flex-row"
+          className="h-12 bg-green-500 rounded-md items-center justify-center flex-row"
           activeOpacity={0.7}
           {...rest}
       >
