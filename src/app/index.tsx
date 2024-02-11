1f683486076111cb483c453efeb4cbecf47bab85
tarushii <tarcisiowesley@gmail.com>
2024-02-11 16:28:25 -0300

    feat: added quantity to header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 
   return (
     <View className="flex-1 pt-8">
-        <Header title="Faça seu pedido" />
+        <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
         <FlatList
             data={CATEGORIES}
