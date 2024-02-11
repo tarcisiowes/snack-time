4fad5112408746b8ae6267c947cdc68a225f7915
tarushii <tarcisiowesley@gmail.com>
2024-02-11 18:14:52 -0300

    styles: changed CategoryButton style

--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -10,10 +10,10 @@ type CategoryButtonProps = PressableProps &{
 export function CategoryButton({title, isSelected, ...rest} : CategoryButtonProps) {
     return (
         <Pressable className={clsx(
-            "bg-amber-400 px-4 h-12 justify-center rounded-md border-2 border-transparent",
-            isSelected && "border-2 border-slate-950"
+            "bg-transparent px-4 h-12 justify-center rounded-md border-b-transparent",
+            isSelected && "border-b-2 border-slate-950 bg-orange-600"
         )} {...rest}>
-            <Text className='text-slate-900 font-subTitle text-sm'>{title}</Text>
+            <Text className={clsx('text-amber-950 font-subTitle text-sm', isSelected && "text-amber-50 text-base")}>{title}</Text>
         </Pressable>
     )
 }
