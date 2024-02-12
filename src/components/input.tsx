0b6a2c268574220674ac9d6becdb8195b7772953
tarushii <tarcisiowesley@gmail.com>
2024-02-12 20:08:35 -0300

    refactor: changed input multiline to single line and adapted to we need for now

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,17 +1,28 @@
 import { TextInput, TextInputProps } from 'react-native';
 import colors from 'tailwindcss/colors';
+import clsx from 'clsx';
 
-export function Input({ ...rest }: TextInputProps) {
+type InputProps = TextInputProps & {
+    placeholderText: string;
+    smallWidth?: boolean;
+    mediumWidth?: boolean;
+};
+
+export function Input({
+    placeholderText,
+    smallWidth = false,
+    mediumWidth = false,
+    ...rest
+}: InputProps) {
     return (
         <TextInput
-            multiline
-            textAlignVertical="top"
-            placeholder=" CEP,
-            Bairro,
-            Rua,
-            Número e complemento"
+            placeholder={placeholderText}
             placeholderTextColor={colors.slate[500]}
-            className="h-32 px-4 py-3 bg-white border-2 border-amber-600 rounded-md text-amber-950 text-sm font-body"
+            className={clsx(
+                'h-16 px-4 py-3 bg-white border-2 border-orange-600 rounded-md text-amber-950 text-base font-body',
+                smallWidth && 'w-1/4',
+                mediumWidth && 'w-2/3 ml-4',
+            )}
             {...rest}
         />
     );
