320018d9abafc50e937d69cb9baf7fbef35b4736
tarushii <tarcisiowesley@gmail.com>
2024-02-13 19:01:58 -0300

    feat: renamed constant backup

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-export const PHONE_NUMBER = '123-456-7890';
+export const PHONE_NUMBER = '0000000000'; // Replace with your phone number with DDD
 
 export const IOS_DEVICE = Platform.OS === 'ios';
 export const ANDROID_DEVICE = Platform.OS === 'android';
