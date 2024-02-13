fd592573c0e3ef486f56b0f290f4e5b8364f3b30
tarushii <tarcisiowesley@gmail.com>
2024-02-13 18:20:59 -0300

    removed phone number

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-export const PHONE_NUMBER = 41987924249;
+export const PHONE_NUMBER = '123-456-7890';
 
 export const IOS_DEVICE = Platform.OS === 'ios';
 export const ANDROID_DEVICE = Platform.OS === 'android';
