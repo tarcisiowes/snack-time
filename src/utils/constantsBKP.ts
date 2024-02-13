cc5c604eadc8af250f332d43dff6f440f1c26bf9
tarushii <tarcisiowesley@gmail.com>
2024-02-13 18:47:57 -0300

    feat: created constant backup

---
+++ src/utils/constantsBKP.ts
@@ -0,0 +1,6 @@
+import { Platform } from 'react-native';
+
+export const PHONE_NUMBER = '123-456-7890';
+
+export const IOS_DEVICE = Platform.OS === 'ios';
+export const ANDROID_DEVICE = Platform.OS === 'android';
