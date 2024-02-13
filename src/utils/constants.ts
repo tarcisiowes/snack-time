c54f4889001603cc9f33167edaa5935e35486087
tarushii <tarcisiowesley@gmail.com>
2024-02-13 18:53:17 -0300

    feat: created constant backup

--- src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-import { Platform } from 'react-native';
-
-export const PHONE_NUMBER = '123-456-7890';
-
-export const IOS_DEVICE = Platform.OS === 'ios';
-export const ANDROID_DEVICE = Platform.OS === 'android';
