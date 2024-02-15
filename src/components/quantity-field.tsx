8bc51252a9b48430b239717cf151ef2f95df40a6
tarushii <tarcisiowesley@gmail.com>
2024-02-14 21:25:48 -0300

    refactor: changed quantity field just to receive params

--- src/components/quantity-field.tsx
+++ src/components/quantity-field.tsx
@@ -1,22 +1,27 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import { ProductCartProps, useCartStore } from '@/data-store/cart-store';
+import { Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Button } from '@/components/button';
 
-export function QuantityField({
-    product,
-    ...rest
-}: {
-    product: ProductCartProps;
-}) {
-    const { remove, add } = useCartStore();
+export function QuantityField({ ...rest }) {
     return (
-        <View>
-            <TouchableOpacity onPress={() => remove(product.id)}>
-                <Text>-</Text>
-            </TouchableOpacity>
-            <Text>{product.quantity}</Text>
-            <TouchableOpacity onPress={() => add(product)}>
-                <Text>+</Text>
-            </TouchableOpacity>
+        <View className="flex-1 flex-row items-center justify-start ml-3">
+            <Button
+                className="bg-transparent w-16"
+                onPress={rest.handleQuantityDecrease}
+            >
+                <Button.Icon>
+                    <Feather name="minus-circle" size={24} color="green" />
+                </Button.Icon>
+            </Button>
+            <Text className="text-xl mx-2">{rest.quantity}</Text>
+            <Button
+                className="bg-transparent w-16"
+                onPress={rest.handleQuantityIncrease}
+            >
+                <Button.Icon>
+                    <Feather name="plus-circle" size={24} color="green" />
+                </Button.Icon>
+            </Button>
         </View>
     );
 }
