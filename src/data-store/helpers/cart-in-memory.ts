f0b65fa7582ed24b3b12818b10f9151cdb8a1d87
tarushii <tarcisiowesley@gmail.com>
2024-02-17 11:00:04 -0300

    refactor: adjusted memory to possibly receive quantity and handle this

--- src/data-store/helpers/cart-in-memory.ts
+++ src/data-store/helpers/cart-in-memory.ts
@@ -4,24 +4,27 @@ import { ProductProps } from '@/utils/data/products';
 export function add(
     products: ProductCartProps[],
     newProduct: ProductProps,
-    quantity: number,
+    quantity?: number,
 ) {
     const existingProduct = products.find(({ id }) => id === newProduct.id);
 
     if (existingProduct) {
         return products.map((product) =>
             product.id === existingProduct.id
-                ? { ...product, quantity: quantity }
+                ? {
+                      ...product,
+                      quantity: (product.quantity || 0) + (quantity || 1),
+                  }
                 : product,
         );
     }
-    return [...products, { ...newProduct, quantity: 1 }];
+    return [...products, { ...newProduct, quantity: quantity || 1 }];
 }
 
 export function remove(
     products: ProductCartProps[],
     productId: string,
-    quantity: number,
+    quantity?: number,
 ) {
     const existingProduct = products.find(({ id }) => id === productId);
 
@@ -30,6 +33,8 @@ export function remove(
     }
 
     return products.map((product) =>
-        product.id === productId ? { ...product, quantity: quantity } : product,
+        product.id === productId
+            ? { ...product, quantity: product.quantity! - 1 }
+            : product,
     );
 }
